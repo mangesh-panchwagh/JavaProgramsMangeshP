750ac033af23ca5e7d83ca7a33ce8671640b0397
Mangesh Panchwagh <mangesh013@gmail.com>
2025-11-19 18:32:09 +0530

    program

--- src/HundredJavaPrograms/J27_ArmstrongNumber.java
+++ src/HundredJavaPrograms/J27_ArmstrongNumber.java
@@ -1,8 +1,14 @@
 package HundredJavaPrograms;
 
+import java.util.Scanner;
+
 public class J27_ArmstrongNumber {
 
 	public static void main(String[] args) {
+		
+		Scanner scan = new Scanner(System.in);
+		System.out.println("Enter the number : ");
+		int num = scan.nextInt();
 		
 		
 	}
---
+++ src/String/Xyz.java
@@ -0,0 +1,12 @@
+package String;
+
+// Print 1 to 100 without using any number
+public class Xyz {
+
+	public static void main(String[] args) {
+		
+		for(int i = 'A' - '@'; i<'e'; i++) {
+			System.out.println(i);
+		}
+	}
+}
